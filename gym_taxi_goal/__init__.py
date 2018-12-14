267e217ee60e64a3b6e5909ec055af4d1064805e
e-dzia <theladyofthegallifrey@gmail.com>
2018-12-14 15:06:01 +0100

    added goal generator to Taxi env

---
+++ gym_taxi_goal/__init__.py
@@ -0,0 +1,9 @@
+from gym.envs.registration import register
+
+from gym_taxi_goal.taxi_goal import TaxiGoalEnv
+
+register(
+    id='TaxiGoal-v0',
+    entry_point='gym_taxi_goal:TaxiGoalEnv',
+    max_episode_steps=200
+)
