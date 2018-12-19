341a98efc2cb76e1eae4ff0e042921a5a99dc322
e-dzia <theladyofthegallifrey@gmail.com>
2018-12-19 11:04:35 +0100

    fixed tests and removed unused code

--- gym_taxi_goal/taxi_goal.py
+++ gym_taxi_goal/taxi_goal.py
@@ -1,17 +1,10 @@
 import gym
 from gym.envs.toy_text.taxi import TaxiEnv
 
-STATE_MOVE_TO_PASSENGER = 1
-STATE_PICKUP_PASSENGER = 2
-STATE_MOVE_TO_DEST = 3
-STATE_DROPOFF_PASSENGER = 4
-STATE_OVER = 5
-
 
 class TaxiGoalEnv(TaxiEnv):
     def __init__(self):
         TaxiEnv.__init__(self)
-        self.goal_generator_state = STATE_OVER
 
     def get_goal_state(self):
         """
@@ -30,25 +23,20 @@ class TaxiGoalEnv(TaxiEnv):
 
         taxiloc = (taxirow, taxicol)
 
-        if self.goal_generator_state == STATE_DROPOFF_PASSENGER:
-            self.goal_generator_state = STATE_OVER
+        if passloc == destidx:
             return None
 
         if passloc == 4:  # it means passenger is in the taxi
             if taxiloc == self.locs[destidx]:  # taxi is in dest
                 passloc = destidx
-                self.goal_generator_state = STATE_DROPOFF_PASSENGER
             else:  # taxi not in dest
                 taxirow = self.locs[destidx][0]
                 taxicol = self.locs[destidx][1]
-                self.goal_generator_state = STATE_MOVE_TO_DEST
         else:  # passenger not in taxi
             if taxiloc == self.locs[passloc]:  # taxi over passenger
                 passloc = 4
-                self.goal_generator_state = STATE_PICKUP_PASSENGER
             else:  # taxi not over passenger
                 taxirow = self.locs[passloc][0]
                 taxicol = self.locs[passloc][1]
-                self.goal_generator_state = STATE_MOVE_TO_PASSENGER
 
         return self.encode(taxirow, taxicol, passloc, destidx)
