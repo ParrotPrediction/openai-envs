43097161f4fe0a8c9c5c672a16b7025a1bb44825
e-dzia <theladyofthegallifrey@gmail.com>
2019-01-08 09:04:52 +0100

    reverted setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
 
-setup(name='openai-envs-taxi',
-      version='2.0.5',
+setup(name='parrotprediction-openai-envs',
+      version='2.0.4',
       description='Custom environments for OpenAI Gym',
       keywords='acs lcs machine-learning reinforcement-learning openai',
-      url='https://github.com/e-dzia/openai-envs',
+      url='https://github.com/ParrotPrediction/openai-envs',
+      author='Parrot Prediction Ltd.',
+      author_email='contact@parrotprediction.com',
       license='MIT',
       packages=find_packages(),
       install_requires=[
